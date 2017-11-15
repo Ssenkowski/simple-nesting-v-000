4ad5d42e5141e481b2267708584d3839185d6f7f
Steven Senkowski <scsenkowski@gmail.com>
2017-11-15 14:01:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -119,6 +119,6 @@ def adding_to_dennis
         }
      }
 		 dennis_ritchies_language = "Assembly"
-programmer_hash[:dennis_ritchie][:languages]<< dennis_ritchies_language
-programmer_hash
+			programmer_hash[:dennis_ritchie][:languages]<< dennis_ritchies_language
+			programmer_hash
 end
