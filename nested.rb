79bb1bbdf912037cf8df8185dafc35530bb98022
Steven Senkowski <scsenkowski@gmail.com>
2017-11-15 13:55:19 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -95,7 +95,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+     alans_new_info = "GUI"
+
 
 end
 
